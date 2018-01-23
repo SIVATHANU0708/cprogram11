9a6b67b803c306e3e6b2fe3c7a49f98c95a04ff2
SIVATHANU0708 <35222125+SIVATHANU0708@users.noreply.github.com>
2018-01-23 22:14:06 +0530

    array.C
    Sum  of first k integers

---
+++ array.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int n[5]={1,2,3,4,5},k,i,sum=0;
+printf("enter the value of k:");
+scanf("%d",&k);
+for(i=0;i<=k;i++)
+{
+sum=sum+n[i];
+}
+printf("the sum of k integer is %d",sum);
+retrun 0;
+}
